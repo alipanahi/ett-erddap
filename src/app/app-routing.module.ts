5f76c469ad8c953349cda06094c17ee7c8da726c
alipanahi <p.alihussain@gmail.com>
2023-04-07 06:06:02 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { PlotComponent } from './components/plot/plot.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main',title:'Main page', component: WrapperComponent },
+  { path: 'plot', title:'Graphs', component: PlotComponent },
+  { path: '',   redirectTo: '/main', pathMatch: 'full' },
+  //{ path: '**', component: WrapperComponent },  // Wildcard route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+import { PlotComponent } from './components/plot/plot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrapperComponent,
+    PlotComponent
   ],
   imports: [
     BrowserModule,
